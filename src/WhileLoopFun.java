9be9c104f857e0a3edb9c3bcfa39a42b8eb40312
gaultl <gaultl@wlhs.wlwv.k12.or.us>
2022-12-06 09:54:20 -0800

    Explored while and do while loops

--- src/WhileLoopFun.java
+++ src/WhileLoopFun.java
@@ -31,6 +31,8 @@ public class WhileLoopFun {
             count ++;
         }
 
+        System.out.println("");
+
         //what does this code loop do? Do this without running it
         count = 2;
         while(count <= 100){
@@ -39,11 +41,25 @@ public class WhileLoopFun {
         }
         System.out.println();
         //write a loop that counts by 5 until you get to 100
-
+        count = 0;
+        while (count <= 100){
+            System.out.print(count + " ");
+            count += 5;
+        }
+        System.out.println(" ");
         //write a loop that counts down from 10 to 1 then prints "Blast off!"
-
+        count = 10;
+        while (count > 0){
+            System.out.print(count + " ");
+            count --;
+        }
+        System.out.print("Blast Off!!");
         //loop mistakes and while vs do while loop
         //make an infinite loop happen
+        count = 5;
+        do{
+            System.out.println("Do something at least once");
+        } while (count >= 10);
 
 
     }
